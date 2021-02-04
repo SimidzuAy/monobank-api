2c32f12a570950e3ff7b1c877148317bcb4d696f
Simidzu-ay <simidzu2ay@protonmail.com>
2021-02-04 18:29:13 +0200

    Statement types

--- src/methods/personal.ts
+++ src/methods/personal.ts
@@ -66,6 +66,70 @@ interface IStatementParams {
     to?: string | number
 }
 
+interface IStatement {
+    /**
+     * Унікальний id транзакції
+     */
+    id: string
+    /**
+     * Час транзакції в секундах в форматі Unix time
+     */
+    time: bigint
+    /**
+     * Опис транзакцій
+     */
+    description: string
+    /**
+     * Код типу транзакції (Merchant Category Code), відповідно ISO 18245
+     */
+    mcc: number
+    /**
+     * Статус блокування суми (детальніше у wiki)
+     * @see {@link https://en.wikipedia.org/wiki/Authorization_hold|wiki}
+     */
+    hold: boolean
+    /**
+     * Сума у валюті рахунку в мінімальних одиницях валюти (копійках, центах)
+     */
+    amount: bigint
+    /**
+     * Сума у валюті транзакції в мінімальних одиницях валюти (копійках, центах)
+     */
+    operationAmount: bigint
+    /**
+     * Код валюти рахунку відповідно ISO 4217
+     */
+    currencyCode: number
+    /**
+     * Розмір комісії в мінімальних одиницях валюти (копійках, центах)
+     */
+    commissionRate: bigint
+    /**
+     * Розмір кешбеку в мінімальних одиницях валюти (копійках, центах)
+     */
+    cashbackAmount: bigint
+    /**
+     * Баланс рахунку в мінімальних одиницях валюти (копійках, центах)
+     */
+    balance: bigint
+    /**
+     * Коментар до переказу, уведений користувачем. Якщо не вказаний, поле буде відсутнім
+     */
+    comment: string
+    /**
+     * Номер квитанции для check.gov.ua
+     */
+    receiptId: string
+    /**
+     * ЄДРПОУ контрагента, присутній лише для елементів виписки рахунків ФОП
+     */
+    counterEdrpou: string
+    /**
+     * IBAN контрагента, присутній лише для елементів виписки рахунків ФОП
+     */
+    counterIban: string
+}
+
 class Personal {
     private readonly _apiUrl: string
     private readonly _token: string
@@ -86,7 +150,7 @@ class Personal {
      * Отримання виписки за час від {from} до {to} часу в секундах в форматі Unix time Максимальний час за який можливо
      * отримувати виписку 31 доба + 1 година (2682000 секунд) Обмеження на використання функції не частіше ніж 1 раз у 60 секунд.
      */
-    public async statement(params: IStatementParams) {
+    public async statement(params: IStatementParams): Promise<IStatement> {
         const maxFrom = 2682000
 
         // Деление на 1000 т.к перевод в секунды...
@@ -96,7 +160,7 @@ class Personal {
         if ( Date.now() - from > maxFrom )
             from = Math.round(Date.now() / 1000 - maxFrom)
 
-        return await makeRequest<Object>(`${this._apiUrl}/personal/statement/${params.account}/${from}/${to}`, this._token)
+        return await makeRequest<IStatement>(`${this._apiUrl}/personal/statement/${params.account}/${from}/${to}`, this._token)
     }
 }
 
